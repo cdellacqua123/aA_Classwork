b2e1951256ec48a5d3c65098716b687317570dd8
Annie Yang <annieqiyang@gmail.com>
2021-06-10 18:39:30 -0400

    Finish Enrollment

--- W5D4/skeleton/app/models/enrollment.rb
+++ W5D4/skeleton/app/models/enrollment.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :integer
+#  student_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
+    belongs_to :user, 
+        primary_key: :id,
+        class_name: "User",
+        foreign_key: :student_id
+    
+    belongs_to :course,
+        primary_key: :id,
+        class_name: "Course",
+        foreign_key: :course_id
 end
--- W5D4/skeleton/app/models/user.rb
+++ W5D4/skeleton/app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_many( 
+        :enrolled_courses, 
+        primary_key: :id,
+        class_name: "Enrollment",
+        foreign_key: :student_id
+    )
+
+    has_many :enrollments, through: :enrollments, source: :course
 end
