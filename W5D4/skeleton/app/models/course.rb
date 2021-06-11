d6e65b9098f32be9d0f1c654140cec85a0cad56f
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-06-10 17:23:59 -0700

    Finished project

--- W5D4/skeleton/app/models/course.rb
+++ W5D4/skeleton/app/models/course.rb
@@ -10,4 +10,26 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+    has_many( 
+        :enrollments, 
+        primary_key: :id,
+        class_name: "Enrollment",
+        foreign_key: :course_id
+    )
+
+    has_many :enrolled_students, through: :enrollments, source: :user
+
+    belongs_to(
+        :prerequisite,
+        primary_key: :id,
+        class_name: "Course",
+        foreign_key: :prereq_id
+    )
+    
+    belongs_to(
+        :instructor,
+        primary_key: :id,
+        class_name: "User",
+        foreign_key: :instructor_id
+    )
 end
--- W5D4/skeleton/app/models/user.rb
+++ W5D4/skeleton/app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
     has_many( 
-        :enrolled_courses, 
+        :enrollments, 
         primary_key: :id,
         class_name: "Enrollment",
         foreign_key: :student_id
     )
 
-    has_many :enrollments, through: :enrollments, source: :course
+    has_many :enrolled_courses, through: :enrollments, source: :course
 end
