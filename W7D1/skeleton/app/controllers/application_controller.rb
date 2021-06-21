8cfacc613c681d01c0e291c5bb001e2fa7d80e0b
Samuel Song <samsongs@umich.edu>
2021-06-21 16:49:46 -0600

    Working on views

--- W7D1/skeleton/app/controllers/application_controller.rb
+++ W7D1/skeleton/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  def log_in(user)
+    session[:session_token] = user.session_token
+  end
+
 end
--- W7D1/skeleton/app/controllers/users_controller.rb
+++ W7D1/skeleton/app/controllers/users_controller.rb
@@ -7,9 +7,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            # Loginto that user
-            log_in(@user) # Write definition in ApplicationController
-            # redirect to that user's page
+            log_in(@user)
             redirect_to user_url(@user)
         else
             render :new
