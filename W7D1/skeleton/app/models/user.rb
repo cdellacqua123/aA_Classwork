02e71bb69991b0cd1a9aa353d6f0a2a281b98578
Samuel Song <samsongs@umich.edu>
2021-06-21 15:42:25 -0600

    Finished user model

--- W7D1/skeleton/app/models/user.rb
+++ W7D1/skeleton/app/models/user.rb
@@ -8,5 +8,31 @@ class User < ApplicationRecord
     def ensure_session_token
         self.session_token ||= SecureRandom::urlsafe_base64
     end
-    
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def check_password(password)
+        password_obj = BCrypt::Password.new(self.password_digest)
+        password_obj.is_password?(password)
+    end
+
+    def self.find_by_credentials(user_name, password)
+        user = User.find_by(user_name: user_name)
+        return nil if user.nil?
+        if user.check_password(password)
+            return user
+        else
+            return nil
+        end
+    end
+
 end
