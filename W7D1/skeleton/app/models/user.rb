3a35851a9309b77670df88cd0ddd974e2e570daa
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-06-21 14:02:00 -0700

    Started project Phase IV

---
+++ W7D1/skeleton/app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :session_token, presence: true, uniqueness: true
+
+    after_initialize :ensure_session_token
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+    
+end
