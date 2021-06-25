788740dfc3ace1e5e8aa9c31f36b466e50edff00
Shirley Tang <shirleytang1996@gmail.com>
2021-06-25 15:05:29 -0400

    finish subs model and controller

---
+++ W7D5/Reddit_Project/app/models/sub.rb
@@ -0,0 +1,5 @@
+class Sub < ApplicationRecord
+    belongs_to :moderator,
+        foreign_key: :user_id,
+        class_name: :User
+end
--- W7D5/Reddit_Project/app/models/user.rb
+++ W7D5/Reddit_Project/app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
     after_initialize :ensure_session_token
     attr_reader :password
 
+    has_many :subs,
+        foreign_key: :user_id,
+        class_name: :Sub
+
     def self.find_by_credentials(username, password)
         user = User.find_by( username: username)
         return nil if user.nil?
