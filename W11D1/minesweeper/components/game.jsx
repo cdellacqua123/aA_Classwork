3334d79aa166ba38c6797c41b23e3894029186cd
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-07-19 17:02:00 -0700

    Worked on phase 4, almost done

--- W11D1/minesweeper/components/game.jsx
+++ W11D1/minesweeper/components/game.jsx
@@ -2,16 +2,24 @@ import React from 'react';
 import * as Minesweeper from "../minesweeper"
 import Board from "./board"
 
+
 class Game extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            board: new Minesweeper.Board(10, 15)
+            board: new Minesweeper.Board(9, 15)
         };
+        this.updateGame = this.updateGame.bind(this);
     };
 
-    updateGame(){
+    updateGame(tile, flagged){
+        if (flagged) {
+            tile.toggleFlag();
+        } else {
+            tile.explore();
+        }
 
+        this.setState({board: this.state.board});
     };
 
     render(){
