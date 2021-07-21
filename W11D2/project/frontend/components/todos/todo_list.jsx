e13da68a4d472e2c4f5bc2364a9bd639a1e7ca91
trustinyoon <trustinyoon@gmail.com>
2021-07-21 16:08:28 -0700

    todolist component rendering

--- W11D2/project/frontend/components/todos/todo_list.jsx
+++ W11D2/project/frontend/components/todos/todo_list.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import { allTodos } from '../../reducers/selectors';
+
+const TodoList = (props) => {
+  const todos = props.todos;
+  
+  return (
+    <div>
+      <h1>All the Todos</h1>
+      <ul>
+        {todos.map(todo => 
+          <li key={todo.id}>
+            Title: {todo.title}
+            <br />
+            Body: {todo.body}
+            <br />
+            Done? {todo.done ? '✅' : '❌'}
+          </li>)}
+      </ul>
+    </div>
+  )
+}
 
 
-
-
-export default () => <h3>Todo List goes here!</h3>
+export default TodoList;
