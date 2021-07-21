596eac8a26af80ec9fb328bd27f720dac4a764be
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-07-21 15:16:02 -0700

    Got through Build It section of Phase 2

--- W11D2/project/frontend/components/app.jsx
+++ W11D2/project/frontend/components/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TodoListContainer from './todos/todo_list_container';
 
 const App = () => {
   return (
     <div>
       <h1>Testing</h1>
+      <TodoListContainer />
     </div>
   );
 }
---
+++ W11D2/project/frontend/components/todos/todo_list.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+
+
+export default () => <h3>Todo List goes here!</h3>
